34aba33babe259838196f62ad50574f472d338ad
sudamghorpade7836 <sudamghorpade1@gmail.com>
2024-01-20 21:49:13 +0530

    test

---
+++ src/main/java/Basics/Features_of_java.java
@@ -0,0 +1,33 @@
+package Basics;
+
+public class Features_of_java {
+	public static void main(String[] args) {
+	/*
+	 Features of Java
+1.Simple: Java is a simple language because its syntax is simple, clean, and easy to understand. 
+Complex and ambiguous concepts of C++ are either eliminated or re-implemented in Java. For example, pointer and 
+operator overloading are not used in Java.
+2.Object-Oriented: In Java, everything is in the form of the object. It means it has some data and behavior.
+ A program must have at least one class and object.
+3.Robust: Java makes an effort to check error at run time and compile time. It uses a strong memory management system called 
+garbage collector. Exception handling and garbage collection features make it strong.
+4.Secure: Java is a secure programming language because it has no explicit pointer and programs runs in the virtual machine.
+ Java contains a security manager that defines the access of Java classes.
+5.Platform-Independent: Java provides a guarantee that code writes once and run anywhere. This byte code is platform-independent
+ and can be run on any machine.
+6.Portable: Java Byte code can be carried to any platform. No implementation-dependent features.
+ Everything related to storage is predefined, for example, the size of primitive data types.
+7.High Performance: Java is an interpreted language. Java enables high performance with the use of the Just-In-Time compiler.
+8.Distributed: Java also has networking facilities. It is designed for the distributed environment
+ of the internet because it supports TCP/IP protocol. It can run over the internet. EJB and RMI are used to create a distributed system.
+9.Multi-threaded: Java also supports multi-threading. It means to handle more than one job a time.	
+	 
+	 */
+		
+		
+		
+		
+		
+	}
+
+}
