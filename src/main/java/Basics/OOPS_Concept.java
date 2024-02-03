c7e6450f753e7d7a0f2a50dcabd48a144d7407b4
sudamghorpade7836 <sudamghorpade1@gmail.com>
2024-02-03 21:46:25 +0530

    test

---
+++ src/main/java/Basics/OOPS_Concept.java
@@ -0,0 +1,17 @@
+package Basics;
+
+public class OOPS_Concept {
+/*
+ OOPs (Object-Oriented Programming System)
+Object means a real-world entity such as a pen, chair, table, computer, watch, etc.
+ Object-Oriented Programming is a methodology or paradigm to design a program using classes and objects. 
+ It simplifies software development and maintenance by providing some concepts:
+
+Object
+Class
+Inheritance
+Polymorphism
+Abstraction
+Encapsulation
+ */
+}
