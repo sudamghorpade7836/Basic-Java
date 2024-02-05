35c70a709cd9618319615a975bc04e031452e151
sudamghorpade7836 <sudamghorpade1@gmail.com>
2024-02-05 22:48:39 +0530

    test

---
+++ src/main/java/Basics/If_statements.java
@@ -0,0 +1,51 @@
+package Basics;
+
+public class If_statements {
+	/*
+	 1) If Statement:
+In Java, the "if" statement is used to evaluate a condition. The control of the program is diverted depending upon the specific condition. The condition of the If statement gives a Boolean value, either true or false. In Java, there are four types of if-statements given below.
+
+Backward Skip 10s
+
+Play Video
+
+Forward Skip 10s
+
+
+Simple if statement
+if-else statement
+if-else-if ladder
+Nested if-statement
+Let's understand the if-statements one by one.
+
+1) Simple if statement:
+It is the most basic statement among all control flow statements in Java. It evaluates a Boolean expression and 
+enables the program to enter a block of code if the expression evaluates to true.
+
+Syntax of if statement is given below.
+
+if(condition) {    
+statement 1; //executes when condition is true   
+}    
+Consider the following example in which we have used the if statement in the java code.
+
+Student.java
+
+Student.java
+
+public class Student {    
+public static void main(String[] args) {    
+int x = 10;    
+int y = 12;    
+if(x+y > 20) {    
+System.out.println("x + y is greater than 20");    
+}    
+}      
+}     
+Output:
+
+x + y is greater than 20
+	 * 
+	 */
+
+}
